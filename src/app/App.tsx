fb754693e1eef510a19cf59244451b69cefb69f3
HowlingWhispers <howlingwhispers@proton.me>
2026-09-14 12:02:48 +0200

    Route world save archives

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,28 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppShell } from '../components/AppShell';
+import { AccountView } from '../views/AccountView';
+import { AdminView } from '../views/AdminView';
 import { AssetDetailView } from '../views/AssetDetailView';
+import { AssetEditorView } from '../views/AssetEditorView';
 import { CollectionView } from '../views/CollectionView';
 import { HomeView } from '../views/HomeView';
 import { NotFoundView } from '../views/NotFoundView';
-import { AccountView } from '../views/AccountView';
-import { VerificationView } from '../views/VerificationView';
-import { AdminView } from '../views/AdminView';
-import { AssetEditorView } from '../views/AssetEditorView';
 import { ProjectView } from '../views/ProjectView';
+import { SaveArchiveView } from '../views/SaveArchiveView';
+import { VerificationView } from '../views/VerificationView';
 
 export function App() {
-  return (
-    <Routes>
-      <Route element={<AppShell />}>
-        <Route index element={<HomeView />} />
-        <Route path="all" element={<CollectionView all />} />
-        <Route path="library/:type" element={<CollectionView />} />
-        <Route path="asset/:id" element={<AssetDetailView />} />
-        <Route path="asset/:id/edit" element={<AssetEditorView />} />
-        <Route path="projects/:slug" element={<ProjectView />} />
-        <Route path="account" element={<AccountView />} />
-        <Route path="verification" element={<VerificationView />} />
-        <Route path="admin" element={<AdminView />} />
-        <Route path="library" element={<Navigate to="/" replace />} />
-        <Route path="*" element={<NotFoundView />} />
-      </Route>
-    </Routes>
-  );
+  return <AppShell><Routes>
+    <Route path="/" element={<HomeView />} />
+    <Route path="/library" element={<Navigate to="/library/worlds" replace />} />
+    <Route path="/library/:collection" element={<CollectionView />} />
+    <Route path="/asset/:id" element={<AssetDetailView />} />
+    <Route path="/asset/:id/saves" element={<SaveArchiveView />} />
+    <Route path="/asset/:id/edit" element={<AssetEditorView />} />
+    <Route path="/project/:slug" element={<ProjectView />} />
+    <Route path="/account" element={<AccountView />} />
+    <Route path="/admin" element={<AdminView />} />
+    <Route path="/verification" element={<VerificationView />} />
+    <Route path="*" element={<NotFoundView />} />
+  </Routes></AppShell>;
 }
