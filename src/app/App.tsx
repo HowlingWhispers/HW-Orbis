bd31d5c2311d63d75c16bb98d5fc8fad6a20e741
HowlingWhispers <howlingwhispers@proton.me>
2026-09-14 12:07:34 +0200

    Fix Save Archive route under AppShell

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,17 +12,22 @@ import { SaveArchiveView } from '../views/SaveArchiveView';
 import { VerificationView } from '../views/VerificationView';
 
 export function App() {
-  return <AppShell><Routes>
-    <Route path="/" element={<HomeView />} />
-    <Route path="/library" element={<Navigate to="/library/worlds" replace />} />
-    <Route path="/library/:collection" element={<CollectionView />} />
-    <Route path="/asset/:id" element={<AssetDetailView />} />
-    <Route path="/asset/:id/saves" element={<SaveArchiveView />} />
-    <Route path="/asset/:id/edit" element={<AssetEditorView />} />
-    <Route path="/project/:slug" element={<ProjectView />} />
-    <Route path="/account" element={<AccountView />} />
-    <Route path="/admin" element={<AdminView />} />
-    <Route path="/verification" element={<VerificationView />} />
-    <Route path="*" element={<NotFoundView />} />
-  </Routes></AppShell>;
+  return (
+    <Routes>
+      <Route element={<AppShell />}>
+        <Route index element={<HomeView />} />
+        <Route path="all" element={<CollectionView all />} />
+        <Route path="library/:type" element={<CollectionView />} />
+        <Route path="asset/:id" element={<AssetDetailView />} />
+        <Route path="asset/:id/saves" element={<SaveArchiveView />} />
+        <Route path="asset/:id/edit" element={<AssetEditorView />} />
+        <Route path="projects/:slug" element={<ProjectView />} />
+        <Route path="account" element={<AccountView />} />
+        <Route path="verification" element={<VerificationView />} />
+        <Route path="admin" element={<AdminView />} />
+        <Route path="library" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFoundView />} />
+      </Route>
+    </Routes>
+  );
 }
